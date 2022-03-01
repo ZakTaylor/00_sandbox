dfb727b358473408eeb68140281138b2c26897ee
taylor.zak <taylor.zak@awatapu.school.nz>
2022-03-02 12:16:52 +1300

    higher lower

---
+++ 06_higher_lower.py
@@ -0,0 +1,21 @@
+
+guess_left = 3
+number = 5
+
+while guess_left > 0:
+    try:
+        guess_number = int(input("Guess a number between 1 and 10! "))
+        guess_left -= 1
+
+        if guess_number == number:
+            print("Correct, {} was the number! ".format(number))
+            break
+        elif number < guess_number:
+            print("Lower! ")
+        elif number > guess_number:
+            print("Higher! ")
+        else:
+            print("Incorrect, you have {} guesses left! ".format(guess_left))
+    except ValueError:
+        print("Please put in a whole number! ")
+
